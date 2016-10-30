cd54a07eb0f66254a679c6799e700b7a38e8cc25
Matt Beland <matt@rearviewmirror.org>
2016-10-29 19:44:14 -0700

    Added comments on makemkv_update_key.sh so I know what I did in a few months.

--- makemkv_update_key.sh
+++ makemkv_update_key.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# 
+# Update MakeMKV Auth Key for Beta - 2016-10-29 by Matt Beland
+#
+# Depends on wget, sed, and html-xml-utils on Ubuntu
+#
+# Grab forum post with Beta key using wget, hxnormalize to parse it, hxselect to grab the div element "codecontent",
+# then sed to strip the <div></div> tags from the returned text of the key. Finally, replace the existing settings
+# file with an updated version using the new key.
+#
+# Runs from crontab.
+#
 key=`echo 'https://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053' | wget -O- -i- -q | hxnormalize -x | hxselect -i "div.codecontent" | sed -e 's/<[^>]*>//g'`
 cat >~/.MakeMKV/settings.conf <<EOL
 #
