8230587cca588a70aed46a4b60e2f31172c07210
Matt Beland <matt@rearviewmirror.org>
2017-01-22 18:28:40 -0800

    Added makehome.sh

---
+++ makehome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git clone matt@odin.rearviewmirror.org:repos/ssh
+git clone matt@odin.rearviewmirror.org:repos/skel
+cd ssh/
+./initiate.sh
+cd skel/
+rm -rf ~/.git*
+rm -f ~/.bashrc
+rm -f ~/.vimrc
+rm -rf ~/.irssi
+mv .bashrc ~/
+mv .vimrc ~/
+mv .irssi ~/
+mv .git* ~/
