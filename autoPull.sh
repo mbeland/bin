5ae45ee824c202dd4e74d6bca25d95a9a0cbd2d8
Matt Beland <matt@rearviewmirror.org>
2017-10-19 11:48:29 -0700

    Removing autoPull.sh

--- autoPull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Automatically update environment repos
-pathList=${1:-"$HOME/.repoList"} 
-while read p; do
-    case "$p" in \#*) continue ;; esac
-    path=~/"$p"
-    /usr/bin/git -C $path pull -q
-done < $pathList
-
